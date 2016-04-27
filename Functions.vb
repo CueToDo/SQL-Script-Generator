9ca1c9d0a1c2c51319ac9c3308fb2b4d4f4ebb90
Alex Cooper - Dell <alex@Q2Do.co.uk>
2016-04-27 09:02:51 +0100

    Remove dependency on second project

---
+++ Functions.vb
@@ -0,0 +1,14 @@
+﻿Public Module Functions
+
+    Public Sub FileWrite(FilenameAbsolute As String, WriteTime As Boolean, Text As String)
+
+        Dim oWrite As New System.IO.StreamWriter(FilenameAbsolute, False)
+
+        oWrite.WriteLine(If(WriteTime, Now.ToString("dd/MM/yyyy HH:mm:ss:fff "), "") & Text)
+
+        oWrite.Flush()
+        oWrite.Close()
+
+    End Sub
+
+End Module
